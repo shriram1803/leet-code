0aa6873aa77125ace44da1cd2d80e40cafcaaab3
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-14 00:12:48 +0530

    Time: 10 ms (43.61%), Space: 9 MB (9.39%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,8 +4,9 @@ public:
         int n = nums.size();
         for(int i = 0; i < n; ++i) {
             bool isPeak = true;
-            if(i > 0 and nums[i] <= nums[i - 1]) isPeak = false;
-            if(i < n - 1 and nums[i] <= nums[i + 1]) isPeak = false;
+            if((i > 0 and nums[i] <= nums[i - 1]) or 
+               (i < n - 1 and nums[i] <= nums[i + 1]))
+                isPeak = false;
             if(isPeak) return i;
         }
         return -1;
