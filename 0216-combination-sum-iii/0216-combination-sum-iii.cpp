2b79814fe6123b3320a2b1f02df72a5e21ca1b51
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-17 15:12:04 +0530

    Time: 3 ms (57.68%), Space: 6.7 MB (31.98%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void solve(vector<int> current, int curr_val, int target, const int k) 
+    void solve(vector<int> current, int curr_val, int curr_sz, int target, const int k) 
     {
-        if(target < 0 or current.size() > k) return;
-        if(target == 0 and current.size() == k) {
-            res.push_back(current);
+        if(curr_sz == k) {
+            if(target == 0) res.push_back(current);
             return;
         }
         for(int i = curr_val; i <= 9; ++i) {
+            if(i > target) break;
             current.push_back(i);
-            solve(current, i + 1, target - i, k);
+            solve(current, i + 1, curr_sz + 1,target - i, k);
             current.pop_back();
         } 
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> v;
-        solve(v, 1, n, k);
+        solve(v, 1, 0, n, k);
         return res;
     }
 };
