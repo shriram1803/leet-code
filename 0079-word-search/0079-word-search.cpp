e8a5423588fae8a36059732f9ab3db1f51908203
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-24 06:26:31 +0530

    Time: 1309 ms (29.82%), Space: 7.9 MB (79.86%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, vector<vector<int>>& dp, string word, int i, int r, int c, const int m, const int n){
+    bool dfs(vector<vector<char>>& board, string word, int i, int r, int c, const int m, const int n){
         if(i >= word.size())
             return true;
-        if(r < 0 or c < 0 or r >= m or c >= n or board[r][c] != word[i] or dp[r][c])
+        if(r < 0 or c < 0 or r >= m or c >= n or board[r][c] != word[i])
             return false;
-        dp[r][c]++;
-        bool res = dfs(board, dp, word, i+1, r + 1,c, m, n) or
-            dfs(board, dp, word, i+1, r - 1, c, m, n) or
-            dfs(board, dp, word, i+1, r ,c + 1, m, n) or
-            dfs(board, dp, word, i+1, r ,c - 1, m, n);
-        dp[r][c]--;
+        board[r][c] = '0';
+        bool res = dfs(board, word, i+1, r + 1,c, m, n) or
+            dfs(board, word, i+1, r - 1, c, m, n) or
+            dfs(board, word, i+1, r ,c + 1, m, n) or
+            dfs(board, word, i+1, r ,c - 1, m, n);
+        board[r][c] = word[i];
         return res;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size(), n = board[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(board[i][j] == word[0]){
-                    if(dfs(board, dp, word, 0, i ,j, m, n))
+                    if(dfs(board, word, 0, i ,j, m, n))
                         return true;
                 }
             }
