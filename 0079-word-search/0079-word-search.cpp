f48858bb59a1dddffd4308972101803c20308e01
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-24 06:12:46 +0530

    Time: 706 ms (50.51%), Space: 8 MB (46.96%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    bool bfs(vector<vector<char>>& board, vector<vector<int>>& dp, string word, int i, int r, int c, const int m, const int n){
+    bool dfs(vector<vector<char>>& board, vector<vector<int>>& dp, string word, int i, int r, int c, const int m, const int n){
         if(i >= word.size())
             return true;
         if(r < 0 or c < 0 or r >= m or c >= n or board[r][c] != word[i] or dp[r][c])
             return false;
         dp[r][c]++;
-        bool res = bfs(board, dp, word, i+1, r + 1,c, m, n) or
-            bfs(board, dp, word, i+1, r - 1, c, m, n) or
-            bfs(board, dp, word, i+1, r ,c + 1, m, n) or
-            bfs(board, dp, word, i+1, r ,c - 1, m, n);
+        bool res = dfs(board, dp, word, i+1, r + 1,c, m, n) or
+            dfs(board, dp, word, i+1, r - 1, c, m, n) or
+            dfs(board, dp, word, i+1, r ,c + 1, m, n) or
+            dfs(board, dp, word, i+1, r ,c - 1, m, n);
         dp[r][c]--;
         return res;
     }
@@ -19,7 +19,7 @@ public:
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(board[i][j] == word[0]){
-                    if(bfs(board, dp, word, 0, i ,j, m, n))
+                    if(dfs(board, dp, word, 0, i ,j, m, n))
                         return true;
                 }
             }
