364726d5d032b6b80d1394416f52fb200a900e77
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-26 15:39:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (50.21%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -3,10 +3,11 @@ public:
     int rotatedDigits(int n) {
         int cnt = 0;
         for(int i = 1; i <= n; ++i) {
-            string s = to_string(i);
+            int temp = i;
             bool isGood = false;
-            for(char ch : s) {
-                int v = ch - '0';
+            while(temp > 0) {
+                int v = temp % 10;
+                temp /= 10;
                 if(v == 2 or v == 5 or v == 6 or v == 9) {
                     isGood = true;
                 } else if(v == 3 or v == 4 or v == 7) {
