b172e2f70e6f4830d9db2e5f1698799b49c5439a
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-28 21:17:57 +0530

    Time: 871 ms (56.00%), Space: 286.7 MB (71.08%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -3,17 +3,22 @@ public:
     vector<int> maxScoreIndices(vector<int>& nums) {
         const int n = nums.size();
         int res = 0, zeroes = 0, ones = 0;
-        unordered_map<int, vector<int>> m;
+        vector<int> v;
         for(int num : nums) if(num == 1) ones++;
         for(int i = 0; i <= n; ++i) {
             int score = zeroes + ones;
+            if(score > res) {
+                v.clear();
+                v.emplace_back(i);
+            } else if(score == res) {
+                v.emplace_back(i);
+            }
             res = max(res, score);
-            m[score].emplace_back(i);
             if(i == n) continue;
             if(nums[i] == 0) zeroes += 1;
             else ones--;
             
         }
-        return m[res];
+        return v;
     }
 };
