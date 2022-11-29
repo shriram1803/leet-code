d93b41d79496e3a6fffbbc01881b417096a8d096
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-29 14:48:11 +0530

    Time: 311 ms (79.78%), Space: 106.1 MB (48.58%) - LeetHub

---
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -0,0 +1,56 @@
+class RandomizedCollection {
+public:
+    map<int, set<int>> m;
+    vector<int> v;
+    int n;
+    RandomizedCollection() {
+        n = 0;
+    }
+    
+    bool insert(int val) {
+        m[val].insert(n++);
+        v.emplace_back(val);
+        if(m[val].size() > 1) {
+            return false;
+        }
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(!m[val].size()) return false;
+        
+        if(val == v.back()) {
+            v.pop_back();
+            m[val].erase(--n);
+            return true;
+        }
+        
+        int val_ind = *m[val].begin();
+        v[val_ind] = v[--n];
+        v.pop_back();
+        // cout<<val_ind<<endl;
+        // cout<<val<< " " <<val_ind <<"   ";
+        // for(auto va : m[val]) cout<<va<<" ";
+        // cout<<endl;
+        
+        int swappedVal = v[val_ind];
+        m[swappedVal].erase(n);
+        m[swappedVal].insert(val_ind);
+        m[val].erase(val_ind);
+        
+        return true;
+    }
+    
+    int getRandom() {
+        int pick = rand() % n;
+        return v[pick];
+    }
+};
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
