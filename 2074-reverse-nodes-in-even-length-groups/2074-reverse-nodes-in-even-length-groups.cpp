49d8b67d2387be9742e8daf3b3fe1865e65f441b
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-29 08:53:56 +0530

    Time: 2570 ms (13.07%), Space: 329.8 MB (61.76%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.cpp
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.cpp
@@ -11,25 +11,16 @@
 class Solution {
 public:
     ListNode* reverseEvenLengthGroups(ListNode* head) {
-        ListNode* curr = head;
-        int n = 0;
+        ListNode* curr = head, * prev = head;
+        int n = 0, grp = 1, visited = 0;
         while(curr) curr = curr->next, n++;
-        int grp = 1, visited = 0;
         curr = head;
-        ListNode* prev = curr;
-        // cout<<n<<endl;
         while(curr and curr->next) {
             int t = grp;
-            // cout<<(visited + grp)<< " " <<curr->val<< endl;
-            // cout<<curr->val<<endl;
             if((grp % 2 and (visited + grp) <= n) or (visited + grp > n and (n - visited)%2 == 1)) {
                 while(--t >= 0 and curr) prev = curr, curr = curr->next;
             } else {
-                // cout<<curr->val<< " "<<prev->val<< endl;
-                // if(visited + grp )
-                // if(grp % 2 == 1 and (n - visited) % 2 == 1) continue;
-                ListNode* save = prev, * pre = curr, * nxt = nullptr;
-                // curr = curr->next;
+                ListNode* save = prev, * nxt = nullptr;
                 while(--t >= 0 and curr) 
                 {
                     nxt = curr->next;
@@ -37,35 +28,15 @@ public:
                     prev = curr;
                     curr = nxt;
                 }
-                // nxt = curr->next;
                 save->next->next = curr;
                 nxt = save->next;
                 save->next = prev;
                 prev = nxt;
-                // curr = pre;
             }
-            // if(curr) cout<<curr->val<<endl;
             visited += grp;
             grp += 1;
         }
-        
-        
-        // int t = n - visited;
-        // if(t > 1) {
-        //     ListNode* save = curr, * pre = curr, * nxt = nullptr;
-        //     curr = curr->next;
-        //     while(--t >= 0 and curr) 
-        //     {
-        //         nxt = curr->next;
-        //         curr->next = pre;
-        //         pre = curr;
-        //         curr = nxt;
-        //     }
-        //     // nxt = curr->next;
-        //     save->next->next = curr;
-        //     save->next = pre;
-        // }
-        
+       
         return head;
     }
 };
