2a76b61ba3a384233dae318ac034a9bbe0204c94
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-11-29 15:24:10 +0530

    Time: 23 ms (75.83%), Space: 14.3 MB (25.35%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,12 +9,11 @@
  */
 class Solution {
 public:
-    TreeNode* res = nullptr;
-    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root) return nullptr;
-        // int local = 0;
-        TreeNode* l = solve(root->left, p, q);
-        TreeNode* r = solve(root->right, p, q);
+        
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
         
         if((l or root == p or root == q) and (r or root == p or root == q)) return root;
         if(l) return l;
@@ -23,37 +22,5 @@ public:
         if(root == p or root == q) return root;
         
         return nullptr;
-        
-//         if((l == p or l == q) and (r == p or r == q)) {
-//             return root;
-//         }
-        
-//         if((root == p or root == q) and (r == p or r == q)) {
-//             return root;
-//         }
-        
-//         if((l == p or l == q) and (root == p or root == q)) {
-//             return root;
-//         }
-        
-//         return nullptr;
-        
-        
-//         if(l == p or l == q) return l;
-        
-//         if(r == p or r == q) return r;
-        
-        // if(root == p or root == q) local++;
-        // local += solve(root->left, p, q) + solve(root->right, p, q);
-        // if(local == 2 and res == nullptr) {
-        //     res = root;
-        // }
-        
-        
-        // return local;
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return solve(root, p, q);
-        return res;
     }
 };
