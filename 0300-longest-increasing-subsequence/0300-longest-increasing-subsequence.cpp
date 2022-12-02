d9965d4f9fea50d36bebef6c15ed64ef446609a1
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-12-02 09:07:48 +0530

    Time: 20 ms (81.87%), Space: 10.4 MB (89.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int ind, int prev, int picks, const int n, vector<vector<int>>& dp) {
-        if(ind == n) {
-            return 0;
-        }
-        if(dp[ind][prev + 1] != -1) return dp[ind][prev + 1]; 
-        int val = solve(nums, ind + 1, prev, picks, n, dp);
-        if(prev == -1 or nums[ind] > nums[prev])
-            val = max(val, 1 + solve(nums, ind + 1, ind, picks + 1, n, dp));
-        dp[ind][prev + 1] = val;
-        return val;
-    }
     int lengthOfLIS(vector<int>& nums) {
-        const int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(nums, 0, -1, 0, n, dp);
+        vector<int> temp;
+        temp.emplace_back(nums[0]);
+        int len = 1, n = nums.size();
+        for(int i = 1; i < n; ++i) {
+            if(nums[i] > temp.back()) {
+                len++;
+                temp.emplace_back(nums[i]);
+            } else {
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[ind] = nums[i];
+            }
+        }
+        return len;
     }
 };
