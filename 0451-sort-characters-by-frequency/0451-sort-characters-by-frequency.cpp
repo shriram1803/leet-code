7114d2b88deef94bb16c98ad46ed39619de30122
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-12-03 18:48:53 +0530

    Time: 25 ms (57.17%), Space: 15.9 MB (5.02%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,17 @@
-typedef pair<int, char> pr;
-
 class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char, int> m;
-        priority_queue<pr> pq;
+        vector<string> bucket(s.size() + 1, "");
         string res = "";
         for(auto ch : s){
             m[ch]++;
         }
         for(auto [element, count] : m){
-            pq.push({count, element});
+            bucket[count].append(count, element);
         }
-        while(!pq.empty()){
-            string temp(pq.top().first, pq.top().second);
-            pq.pop();
-            res += temp;
+        for(int i = s.size(); i > 0; --i) {
+            res += bucket[i];
         }
         return res;
     }
