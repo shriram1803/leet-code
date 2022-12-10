d64603ffe9a254ef78b47d73d0a41fb1d83f562a
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-12-10 19:07:54 +0530

    Time: 51 ms (90.86%), Space: 16 MB (72.09%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& matrix, vector<vector<int>> &dp, int r, int c, int prev) {
-        
-        
-        // cout << r << c << endl;
-        int m = matrix.size(), n = matrix[0].size();
+    int solve(vector<vector<int>>& matrix, vector<vector<int>> &dp, int r, int c, int prev, const int m, const int n) {
         if(r < 0 or c < 0 or r >= m or c >= n or matrix[r][c] <= prev) return 0;
         if(dp[r][c] != -1) return dp[r][c];
         dp[r][c] = 0;
         int res = 1 + max({
-           solve(matrix, dp, r + 1, c, matrix[r][c]),
-           solve(matrix, dp, r - 1, c, matrix[r][c]),
-           solve(matrix, dp, r, c + 1, matrix[r][c]),
-           solve(matrix, dp, r, c - 1, matrix[r][c]) 
+           solve(matrix, dp, r + 1, c, matrix[r][c], m, n),
+           solve(matrix, dp, r - 1, c, matrix[r][c], m, n),
+           solve(matrix, dp, r, c + 1, matrix[r][c], m, n),
+           solve(matrix, dp, r, c - 1, matrix[r][c], m, n) 
         });
         dp[r][c] = res;
         return res;
@@ -23,15 +19,9 @@ public:
         vector<vector<int>> dp(m, vector<int>(n, -1));
         for(int i = 0; i < m; ++i) {
             for(int j = 0; j < n; ++j) {
-                res = max(res, solve(matrix, dp, i, j, INT_MIN));
+                res = max(res, solve(matrix, dp, i, j, INT_MIN, m, n));
             }
         }
-        // for(int i = 0; i < m; ++i) {
-        //     for(int j = 0; j < n; ++j) {
-        //         cout << dp[i][j] << " ";
-        //     }
-        // }
-        // cout << endl;
         return res;
     }
 };
