7e141b1beeb0478272aa41957a1a8c0b1024265c
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-12-14 21:10:20 +0530

    Time: 2 ms (60.85%), Space: 7.8 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,11 +3,10 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        int prev = nums[0], pre = max(nums[1], nums[0]), temp;
+        nums[1] = max(nums[0], nums[1]);
         for(int i = 2; i < n; ++i) {
-            temp = max(pre, nums[i] + prev);
-            prev = pre, pre = temp;
+            nums[i] = max(nums[i - 1], nums[i] + nums[i - 2]);
         }
-        return pre;
+        return nums[n - 1];
     }
 };
