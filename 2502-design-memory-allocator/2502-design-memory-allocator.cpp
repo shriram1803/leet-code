ce56d62ca135f01df67ed97d241128dfd8ae311e
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-12-18 16:01:15 +0530

    Time: 285 ms (40.03%), Space: 30.8 MB (52.68%) - LeetHub

---
+++ 2502-design-memory-allocator/2502-design-memory-allocator.cpp
@@ -0,0 +1,44 @@
+class Allocator {
+public:
+    vector<int> v;
+    int N;
+    unordered_map<int, int> m;
+    Allocator(int n) {
+        N = n;
+        v.assign(N, -1);
+    }
+    
+    int allocate(int size, int mID) {
+        int k = 0;
+        for(int i = 0; i < N; ++i) {
+            if(v[i] == -1) {
+                ++k;
+                if(k == size) {
+                    fill_n(v.begin() + (i - k + 1), k, mID);
+                    return i - k + 1;
+                }
+            } else {
+                k = 0;
+            }
+        }
+        return -1;
+    }
+    
+    int free(int mID) {
+        int cnt = 0;
+        for(auto& val : v) {
+            if(val == mID) {
+                val = -1;
+                ++cnt;
+            }
+        }
+        return cnt;
+    }
+};
+
+/**
+ * Your Allocator object will be instantiated and called as such:
+ * Allocator* obj = new Allocator(n);
+ * int param_1 = obj->allocate(size,mID);
+ * int param_2 = obj->free(mID);
+ */
