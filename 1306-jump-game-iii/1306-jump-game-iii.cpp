98f0b0ae473216c609d0da4d906340c3877f7949
Shriram R <83195038+shriram1803@users.noreply.github.com>
2022-12-26 16:02:18 +0530

    Time: 51 ms (85.83%), Space: 30.7 MB (98.68%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    bool solve(vector<int>& arr, int start, vector<int>& vis) {
-        if(start >= arr.size() or start < 0 or vis[start]) return false;
-        if(arr[start] == 0) return true;
-        ++vis[start];
-        return solve(arr, start + arr[start], vis) or solve(arr, start - arr[start], vis);
-        
-    }
     bool canReach(vector<int>& arr, int start) {
-        vector<int> visited(arr.size(), 0);
-        return solve(arr, start, visited);   
+        if(start >= arr.size() or start < 0 or arr[start] < 0) return false;
+        if(arr[start] == 0) return true;
+        arr[start] = -arr[start];
+        return canReach(arr, start + arr[start]) or canReach(arr, start - arr[start]); 
     }
 };
