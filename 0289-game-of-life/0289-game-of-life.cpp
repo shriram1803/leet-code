87a41c0e0efe2fbcd28f23d54371d4d852c04d71
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-01-02 14:50:41 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (23.80%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int isLive(vector<vector<int>>& temp, int m, int n, int i, int j) {
+    int isLive(vector<vector<int>>& board, int m, int n, int i, int j) {
         if(i < 0 or j < 0 or i >= m or j >= n) return 0;
-        if(temp[i][j] == 1) return 1;
+        if(board[i][j] == 1 or board[i][j] == 2) return 1;
         return 0;
     }
     void gameOfLife(vector<vector<int>>& board) {
@@ -10,15 +10,30 @@ public:
         vector<vector<int>> dirs{
             {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}
         };
-        vector<vector<int>> temp = board;
         for(int i = 0; i < m; ++i) {
             for(int j = 0; j < n; ++j) {
                 int cnt = 0;
                 for(auto& dir : dirs) {
-                    cnt += isLive(temp, m, n, i + dir[0], j + dir[1]);
+                    cnt += isLive(board, m, n, i + dir[0], j + dir[1]);
                 }
-                if(cnt == 3 or (temp[i][j] == 1 and cnt == 2)) board[i][j] = 1;
-                else board[i][j] = 0;
+                if(cnt == 3) {
+                    if(board[i][j] == 0) {
+                        board[i][j] = 3;
+                    }
+                } else if(cnt != 2) {
+                    if(board[i][j] == 1) {
+                        board[i][j] = 2;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(board[i][j] == 3) {
+                    board[i][j] = 1;
+                } else if(board[i][j] == 2) {
+                    board[i][j] = 0;
+                }
             }
         }
     }
