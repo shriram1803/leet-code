ba1a62067117be5abe480f64e8f91b0df7c46bbe
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-01-04 15:14:40 +0530

    Time: 458 ms (28.57%), Space: 84.2 MB (54.96%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
         int n = heights.size();
-        vector<int> res(n, 0), stack;
+        vector<int> res(n, 0);
+        stack<int> st;
         for(int i = n - 1; i >= 0; --i) {
             int cnt = 0;
-            while(!stack.empty() and stack.back() < heights[i]) ++cnt, stack.pop_back();
-            if(!stack.empty()) ++cnt;
+            while(!st.empty() and st.top() < heights[i]) ++cnt, st.pop();
+            if(!st.empty()) ++cnt;
             res[i] = cnt;
-            stack.emplace_back(heights[i]);
+            st.push(heights[i]);
         }
         return res;
     }
