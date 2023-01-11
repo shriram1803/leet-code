99f04e85ced41f3aa69ce9b3e407cf437fe11f94
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-01-11 14:29:08 +0530

    Time: 175 ms (95.30%), Space: 61 MB (51.34%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int solve(int i, int n, vector<vector<int>>& v, vector<bool>& has, vector<int>& vi) {
         if(vi[i]) return 0;
-        vi[i] = 1;
         int res = 0;
+        vi[i] = 1;
         for(int val : v[i]) {
             res += solve(val, n, v, has, vi);            
         }
+        vi[i] = 0;
         if(i != 0 and (has[i] or res > 0)) {
             ++res;
         }
-        vi[i] = 0;
         return res;
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
         vector<vector<int>> v(n);
         vector<int> vis(n, 0);
         for(auto& edge : edges) {
-            int x = edge[0], y = edge[1];
-            v[x].emplace_back(y);
-            v[y].emplace_back(x);
+            v[edge[0]].emplace_back(edge[1]);
+            v[edge[1]].emplace_back(edge[0]);
         }
-        int val = solve(0, n, v, hasApple, vis);
-        return 2 * val;
+        return 2 * solve(0, n, v, hasApple, vis);
     }
 };
