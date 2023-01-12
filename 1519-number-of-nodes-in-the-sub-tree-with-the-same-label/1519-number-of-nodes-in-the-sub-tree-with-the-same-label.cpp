7d7efa1ea8e3304cb5d1f26ce7e45c046ad5dbde
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-01-12 06:58:36 +0530

    Time: 448 ms (97.04%), Space: 160.5 MB (99.26%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    void solve(vector<int>& res, int i, vector<vector<int>>& g, string& labels, int freq[]) 
+    void solve(vector<int>& res, int i, int n, vector<vector<int>>& g, string& labels, vector<bool>& vi, vector<int>& freq) 
     {
-        if(res[i] == 0) {
-            res[i] = 1;
-            for(int val : g[i]) {
-                int freq1[26] = {0};
-                solve(res, val, g, labels, freq1);
-                for(int j = 0; j < 26; ++j) {
-                    freq[j] += freq1[j];
-                }
-            }
-            res[i] = ++freq[labels[i] - 'a'];
+        if(vi[i]) return;
+        vi[i] = true;
+        int asci = labels[i] - 'a';
+        ++freq[asci];
+        int curr = freq[asci];
+        int tot = 0;
+        for(int val : g[i]) {
+            if(val == i) continue;
+            solve(res, val, n, g, labels, vi, freq);
+            tot += freq[asci] - curr;
+            res[i] += freq[asci] - curr;
+            freq[asci] = curr;
         }
+        freq[asci] = curr + tot;
+        vi[i] = false;
     }
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-        vector<int> res(n, 0);
-        int freq[26] = {0};
+        vector<int> res(n, 1), freq(26, 0);
+        vector<bool> visited(n, false);
         vector<vector<int>> graph(n);
         for(auto& ed : edges) {
             graph[ed[0]].emplace_back(ed[1]);
             graph[ed[1]].emplace_back(ed[0]);
         }
-        solve(res, 0, graph, labels, freq);
+        solve(res, 0, n, graph, labels, visited, freq);
         return res;
     }
 };
