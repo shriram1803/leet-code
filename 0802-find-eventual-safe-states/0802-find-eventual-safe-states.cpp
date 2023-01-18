275f582fc9c9aa858a0ee4b1e5ea58d0a5e92fd3
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-01-18 18:09:03 +0530

    Time: 175 ms (73.40%), Space: 47.7 MB (74.37%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& g, vector<int>& vis, vector<int>& path, int i, vector<int>& isCycle) {
-        if(path[i] or isCycle[i]) return true;
+    bool dfs(vector<vector<int>>& g, vector<int>& vis, vector<int>& path, int i) {
+        if(path[i]) return true;
         if(vis[i]) return false;
         path[i] = 1, vis[i] = 1;
         bool res = false;
         for(int val : g[i]) {
-            if(dfs(g, vis, path, val, isCycle)) {
+            if(dfs(g, vis, path, val)) {
                 res = true;
-                isCycle[val] = 1;
             }
         }
+        if(res) return true;
         path[i] = 0;
-        return res;
+        return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> path(n, 0), vis(n, 0), res;
-        vector<int> isCycle(n, 0);
         for(int i = 0; i < n; ++i) {
             if(!vis[i])
-                if(dfs(graph, vis, path, i, isCycle))
-                    isCycle[i] = 1;
+                dfs(graph, vis, path, i);
         }
         for(int i = 0; i < n; ++i) {
-            if(isCycle[i]) continue;
+            if(path[i]) continue;
             res.emplace_back(i);
         }
         return res;
