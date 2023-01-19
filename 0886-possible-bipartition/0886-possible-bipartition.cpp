a92a4d0a26c63c1a50c65d901fbc369cd83444a9
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-01-19 21:18:05 +0530

    Time: 184 ms (98.81%), Space: 65.1 MB (55.94%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, vector<int>& vis, vector<int>& color, int curr, int prev) {        
-        if(vis[curr] == 1) {
-            if(color[curr] == prev)
-                return true;
-            return false;
-        }
-        vis[curr] = 1, color[curr] = !prev;
+    bool dfs(vector<vector<int>>& adj, vector<int>& color, int curr) {   
         for(int node : adj[curr]) {
-            if(node == curr) continue;            
-            if(dfs(adj, vis, color, node, color[curr]))
+            if(color[node] == color[curr])
+                return true;
+            if(color[node] != -1) continue;
+            color[node] = !color[curr];
+            if(dfs(adj, color, node))
                 return true;
         }
         return false;
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<vector<int>> adj(n + 1);
-        vector<int> vis(n + 1, 0), color(n + 1, -1);
+        vector<int> color(n + 1, -1);
         for(auto &d : dislikes) {
             adj[d[0]].emplace_back(d[1]);
             adj[d[1]].emplace_back(d[0]);
         }
         for(int i = 1; i <= n; ++i) {
-            if(vis[i] == 1) continue;
-            if(dfs(adj, vis, color, i, 1))
+            if(color[i] != -1) continue;
+            color[i] = 0;
+            if(dfs(adj, color, i))
                 return false;
         }
         return true;
