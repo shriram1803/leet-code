000635200040a3cd69fda6a1d6723beab5b977a6
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-01-19 12:43:43 +0530

    Time: 12 ms (98.80%), Space: 14.5 MB (96.26%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        vector<int> res(n, 101);
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto& e : redEdges) {
+            adj[e[0]].push_back({e[1], 0});
+        }
+        for(auto& e : blueEdges) {
+            adj[e[0]].push_back({e[1], 1});
+        }
+        deque<array<int, 3>> dq;
+        res[0] = 0;
+        dq.push_back({0, -1, 0});
+        while(!dq.empty()) {
+            auto [node, color, d] = dq.front();
+            dq.pop_front();
+            if(d < res[node]) res[node] = d;
+            for(auto& it : adj[node]) {
+                if(it.second != color and it.first != -1) {
+                    dq.push_back({it.first, it.second, d + 1});
+                    it.first = -1;
+                }
+            }            
+        }
+        for(int i = 0; i < n; ++i) {
+            if(res[i] == 101)
+                res[i] = -1;
+        }
+        return res;
+    }
+};
