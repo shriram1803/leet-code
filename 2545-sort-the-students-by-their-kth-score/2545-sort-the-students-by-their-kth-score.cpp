19049fab6713437e1acb02f96fb9df9dc3004e05
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-01-22 10:38:18 +0530

    Time: 58 ms (100.00%), Space: 27.5 MB (30.77%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
         sort(score.begin(), score.end(), 
-             [&](vector<int> & a, vector<int> & b) {
+             [&](auto const & a, auto const & b) {
                  return a[k] > b[k];
              }
             );
