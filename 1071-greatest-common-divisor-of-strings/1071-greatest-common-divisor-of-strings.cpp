28fae5d07fd66c339d28cae4b530483ccf7359e2
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-02-01 14:14:41 +0530

    Time: 7 ms (38.39%), Space: 9.4 MB (12.68%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -8,15 +8,16 @@ public:
     }
     string gcdOfStrings(string str1, string str2) {
         string res = "";
-        string merge = str1 + str2, curr = "";
+        string merge = str1 + str2, curr = "", curr1 = "";
         int n1 = str1.size(), n2 = str2.size();
         for(int i = 0; i < min(str1.size(), str2.size()); ++i) {
-            curr += merge[i];
+            curr += str1[i], curr1 += str2[i];
             if(n1 % (i + 1) != 0 or n2 % (i + 1) != 0) continue;
-            if(repeat(curr, (n1 + n2) / (i + 1)) == merge) {
+            if(curr != curr1) return res;
+            if(repeat(curr, n1 / (i + 1)) == str1 and repeat(curr, n2 / (i + 1)) == str2){
                 res = curr;
-            }   
-        }        
+            }
+        }  
         return res;
     }
 };
