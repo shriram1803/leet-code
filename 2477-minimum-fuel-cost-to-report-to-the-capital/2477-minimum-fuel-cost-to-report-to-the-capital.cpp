f6a875a574659684c28a2cd29c8b287061e16fd7
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-02-12 21:50:13 +0530

    Time: 714 ms (47.24%), Space: 229.3 MB (15.27%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    long long res = 0;
-    int dfs(unordered_map<int, vector<int>>& m, int curr, int prev, int seats) {
+    long long res = 0; double SEATS;
+    int dfs(unordered_map<int, vector<int>>& m, int curr, int prev) {
         
         int children = 1;
         
         for(int child : m[curr]) {
             if(child == prev) continue;
-            int val = dfs(m, child, curr, seats);
-            res += ceil(val / (double)seats);
+            int val = dfs(m, child, curr);
+            res += ceil(val / SEATS);
             children += val;
         }
         
@@ -17,11 +17,12 @@ public:
     }
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
         unordered_map<int, vector<int>> m;
+        SEATS = seats;
         for(auto& road : roads) {
             m[road[0]].emplace_back(road[1]);
             m[road[1]].emplace_back(road[0]);
         }
-        dfs(m, 0, -1, seats);
+        dfs(m, 0, -1);
         return res;
     }
 };
