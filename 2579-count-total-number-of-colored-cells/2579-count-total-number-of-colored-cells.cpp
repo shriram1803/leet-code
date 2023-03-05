f8be6bb674c93dcacdd49bf0c129d7b3d8bfebb8
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-03-05 12:43:03 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (100.00%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     long long coloredCells(int n) {
         
-        long long start = 1, diff = 2;
-        
-        long long res = (n * (2 * start + (n - 1) * diff)) / 2;
+        long long res = (n * ((long long)2 + (n - (long long)1) * (long long)2)) / (long long)2;
         
         res = (res * 2) - (2 * n - 1);
         
