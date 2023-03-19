20af7108a57ea8df88ee97b3b122ce392e1e861d
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-03-19 19:15:46 +0530

    Time: 15 ms (66.67%), Space: 29.1 MB (33.33%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> moves{
+        {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}
+    };
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int x = 0, y = 0, curr = 0;
+        int visited = 1;
+        while(visited < n * n) {
+            bool found = false;
+            for(int i = 0; i < 8; ++i) {
+                int nextX = x + moves[i][0], nextY = y + moves[i][1];
+                if(nextX < 0 or nextY < 0 or nextX >= n or nextY >= n) 
+                    continue;
+                if(grid[nextX][nextY] == curr + 1) {
+                    found = true;
+                    x = nextX, y = nextY;
+                    break;
+                } 
+            }
+            if(!found) {
+                return false;
+            }
+            curr += 1;
+            visited += 1;
+        }
+        return true;
+    }
+};
