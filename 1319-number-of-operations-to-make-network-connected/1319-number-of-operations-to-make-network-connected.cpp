d5eca4f321db27c87803b7de5d1fecd8b4b410c8
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-03-23 12:04:12 +0530

    Time: 148 ms (48.60%), Space: 51.5 MB (39.90%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    int cycles = 0;
-    void dfs(vector<vector<int>>& adj, vector<int>& vis, vector<int>& path, int curr, int prev) {
-        if(path[curr]) {
-            cycles += 1;
-            return;
+    void dfs(vector<vector<int>>& adj, vector<int>& vis, int node) {
+        if(vis[node]) return;
+        vis[node] = 1;
+        for(int n : adj[node]) {
+            dfs(adj, vis, n);
         }
-        if(vis[curr]) {
-            return;
-        }
-        vis[curr] = 1, path[curr] = 1;
-        for(int next : adj[curr]) {
-            if(next != prev)
-                dfs(adj, vis, path, next, curr);
-        }
-        path[curr] = 0;
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size() < n - 1) 
+            return -1;
         vector<vector<int>> adj(n);
-        vector<int> vis(n, 0), path(n, 0);
-        for(auto& conn : connections) {
-            adj[conn[0]].emplace_back(conn[1]);
-            adj[conn[1]].emplace_back(conn[0]);
-        }
+        vector<int> vis(n, 0);
         int comps = 0;
+        for(auto& con : connections) {
+            adj[con[0]].emplace_back(con[1]);
+            adj[con[1]].emplace_back(con[0]);
+        }
         for(int i = 0; i < n; ++i) {
-            if(vis[i] == 0) {
-                dfs(adj, vis, path, i, -1);
-                comps += 1;
+            if(!vis[i]) {
+                ++comps;
+                dfs(adj, vis, i);
             }
         }
-        if(cycles < comps - 1)
-            return -1;
         return comps - 1;
     }
 };
