e88b4f24cc8a63de389f3e8179962ecd74b91e9b
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-03-26 11:59:12 +0530

    Time: 177 ms (92.88%), Space: 96.4 MB (75.42%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -15,7 +15,7 @@ public:
     }
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        vector<int> vis(n, 0), path(n, -1);
+        vector<int> path(n, -1);
         for(int i = 0; i < n; ++i) {
             if(edges[i] != -1)
                 dfs(edges, path, i, 0);
