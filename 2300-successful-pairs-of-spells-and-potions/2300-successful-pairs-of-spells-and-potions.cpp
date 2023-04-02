9b77b6abfe2666ce828e2cdd42eec563dae3a32b
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-04-02 09:28:57 +0530

    Time: 406 ms (5.74%), Space: 133.8 MB (5.08%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         set<int> s(spells.begin(), spells.end());
         unordered_map<int, int> resTable;
-        vector<int> res(spells.size());
         sort(potions.begin(), potions.end(), greater<int>());
         int ind = 0, n = potions.size();
         for(auto& it : s) {
