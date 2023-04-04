feaedc230f1a096fb67ac39e5f2c335b3bc78ff6
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-04-04 10:48:51 +0530

    Time: 28 ms (83.48%), Space: 10.4 MB (77.04%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int partitionString(string s) {
-        unordered_set<char> st;
+        vector<int> freq(26, 0);
         int partitions = 1;
         for(auto ch : s) {
-            if(st.count(ch)) {
-                st.clear();
+            if(freq[ch - 'a'] > 0) {
                 partitions += 1;
+                fill(freq.begin(), freq.end(), 0);
             }
-            st.insert(ch);
+            freq[ch - 'a'] += 1;
         }
         return partitions;
     }
