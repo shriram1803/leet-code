d69289eaed362f3de0ccd8d8080b5812f1c3ea4f
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-04-30 08:16:19 +0530

    Time: 44 ms (75.00%), Space: 81.1 MB (87.50%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -5,15 +5,11 @@ public:
         vector<int> freq(n + 1, 0), res(n, 0);
         for(int i = 0; i < n; ++i) {
             freq[A[i]] += 1;
-            freq[B[i]] += 1;
-            if(A[i] == B[i]) {
+            if(freq[A[i]] == 2)
                 common_cnt += 1;
-            } else {
-                if(freq[A[i]] == 2)
-                    common_cnt += 1;
-                if(freq[B[i]] == 2)
-                    common_cnt += 1;
-            }
+            freq[B[i]] += 1;
+            if(freq[B[i]] == 2)
+                common_cnt += 1;
             res[i] = common_cnt;
         }
         return res;
