262939f22c3c4a9e7ac29aa7e125d4e755a68cc0
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-05-05 10:07:58 +0530

    Time: 59 ms (7.13%), Space: 10.1 MB (29.12%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -5,10 +5,8 @@ public:
         int max_vow_cnt = 0;
         unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u'};
         for(int i = 0; i < s.size(); ++i) {
-            if(i >= k) {
-                if(vowels.count(s[i - k])) 
-                    vow_cnt -= 1;
-            }
+            if(i >= k and vowels.count(s[i - k]))
+                vow_cnt -= 1;
             if(vowels.count(s[i]))
                 vow_cnt += 1;
             max_vow_cnt = max(max_vow_cnt, vow_cnt);
