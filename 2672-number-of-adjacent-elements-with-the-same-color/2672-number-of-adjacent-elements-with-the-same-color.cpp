d4a9766f0ddd06840c0b0b74a51b030f9d33cba0
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-05-07 15:22:52 +0530

    Time: 592 ms (66.67%), Space: 171.9 MB (66.67%) - LeetHub

--- 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.cpp
+++ 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    void handle(vector<int>& nums, int& curr, int ind, int col, int op) {
-        if(ind > 0 and nums[ind - 1] == col)
-            curr += op;
-        if(ind < nums.size() - 1 and nums[ind + 1] == col)
-            curr += op;
-    }
     vector<int> colorTheArray(int n, vector<vector<int>>& queries) {
-        vector<int> res(queries.size()), nums(n, 0);
+        vector<int> res(queries.size()), nums(n + 2, 0);
         int curr = 0;
         for(int i = 0; i < queries.size(); ++i) {
-            int ind = queries[i][0], col = queries[i][1];
+            int ind = queries[i][0] + 1, col = queries[i][1];
             if(nums[ind] != 0)
-                handle(nums, curr, ind, nums[ind], -1);
+                curr -= ((nums[ind - 1] == nums[ind]) + (nums[ind] == nums[ind + 1]));
             nums[ind] = col;
-            handle(nums, curr, ind, col, 1);            
+            curr += ((nums[ind - 1] == nums[ind]) + (nums[ind] == nums[ind + 1]));     
             res[i] = curr;
         }
         return res;
