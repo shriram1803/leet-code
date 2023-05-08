a1d517e8a890f4dcbd0ffbae60779bfc86ce9a09
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-05-08 11:14:45 +0530

    Time: 459 ms (100.00%), Space: 200.9 MB (57.14%) - LeetHub

---
+++ 2671-frequency-tracker/2671-frequency-tracker.cpp
@@ -0,0 +1,33 @@
+class FrequencyTracker {
+public:
+    unordered_map<int, int> fq, numfq;
+    FrequencyTracker() {
+        
+    }
+    
+    void add(int number) {
+        fq[number] += 1;
+        numfq[fq[number]] += 1;
+        numfq[fq[number] - 1] -= 1; 
+    }
+    
+    void deleteOne(int number) {
+        if(fq[number] > 0) {
+            numfq[fq[number]] -= 1;
+            numfq[fq[number] - 1] += 1;
+            fq[number] -= 1;
+        }
+    }
+    
+    bool hasFrequency(int frequency) {
+        return numfq[frequency] > 0;
+    }
+};
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker* obj = new FrequencyTracker();
+ * obj->add(number);
+ * obj->deleteOne(number);
+ * bool param_3 = obj->hasFrequency(frequency);
+ */
