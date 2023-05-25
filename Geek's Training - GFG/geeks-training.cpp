713e1ac58710fcce0332ade1e107c561a6ef65b8
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-05-25 13:02:22 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -5,22 +5,22 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int f(vector<vector<int>>& p, int day, int last, vector<vector<int>>& dp) {
-      if(day < 0) return 0;
-      if(dp[day][last] != -1) return dp[day][last];
-      int maxx = 0;
-      for(int i = 0; i < 3; ++i) {
-          if(i != last) {
-              maxx = max(maxx, p[day][i] + f(p, day - 1, i, dp));
-          }
-      }
-      dp[day][last] = maxx;
-      return dp[day][last];
-  }
     int maximumPoints(vector<vector<int>>& points, int n) {
         // Code here
-        vector<vector<int>> dp(n, vector<int>(4, -1));
-        return f(points, n - 1, 3, dp);
+        vector<vector<int>> dp(n, vector<int>(3, -1));
+        dp[0][0] = points[0][0];
+        dp[0][1] = points[0][1];
+        dp[0][2] = points[0][2];
+        for(int i = 1; i < n; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                for(int k = 0; k < 3; ++k) {
+                    if(j != k) {
+                        dp[i][j] = max(dp[i][j], points[i][j] + dp[i - 1][k]);
+                    }
+                }
+            }
+        }
+        return max(dp[n - 1][0], max(dp[n - 1][1], dp[n - 1][2]));
     }
 };
 
