0f45cf6ed63117b4439eabe3a5f6da31ff899996
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-05-28 18:43:04 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -23,8 +23,30 @@ class Solution{
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
-        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-        return f(sum, n - 1, arr, dp);
+        
+        // vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
+        
+        vector<vector<int>> dp(n, vector<int>(sum + 1, 0));
+        
+        // if(arr[0] == 0) dp[0][0] += 1;
+        // if(arr[0] <= sum) dp[0][arr[0]] += 1;
+        if(arr[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+        if(arr[0] != 0 and arr[0] <= sum) dp[0][arr[0]] = 1;
+        
+        for(int i = 1; i < n; ++i) {
+            for(int j = 0; j <= sum; ++j) {
+                int not_take = dp[i - 1][j];
+                int take = 0;
+                if(j >= arr[i])
+                    take += dp[i - 1][j - arr[i]];
+                dp[i][j] += (take + not_take) % mod;
+            }
+        }
+        
+        // return f(sum, n - 1, arr, dp);
+        
+        return dp[n - 1][sum];
 	}
 	  
 };
