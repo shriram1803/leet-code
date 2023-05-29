356e7cf9d0fe6575b363b06d2d750b368a069edb
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-05-29 14:24:42 +0530

    Time: 178 ms (16.36%), Space: 45.1 MB (36.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int backtrack(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp) {        
-        if(ind == 0) {
-            if(amt % coins[0] == 0)
-                return amt / coins[0];
-            return 1e9;
-        }
-        
-        if(dp[ind][amt] != -1) 
-            return dp[ind][amt];
-        
-        int not_pick = backtrack(ind - 1, amt, coins, dp);;
-        int pick = 1e9;
-        if(amt >= coins[ind])
-            pick = 1 + backtrack(ind, amt - coins[ind], coins, dp);
-        
-        return dp[ind][amt] = min(not_pick, pick);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int res = backtrack(n - 1, amount, coins, dp);
-        if(res == 1e9)
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        
+        for(int i = 0; i <= amount; ++i) {
+            if(i % coins[0] == 0)
+                dp[0][i] = i / coins[0];
+            else
+                dp[0][i] = 1e9;
+        }
+        
+        for(int ind = 1; ind < n; ++ind) {
+            for(int amt = 0; amt <= amount; ++amt) {
+                int not_pick = dp[ind - 1][amt];
+                int pick = 1e9;
+                if(amt >= coins[ind])
+                    pick = 1 + dp[ind][amt - coins[ind]];
+                dp[ind][amt] = min(not_pick, pick);
+            }
+        }
+        
+        if(dp[n - 1][amount] == 1e9)
             return -1;
-        return res;
+        return dp[n - 1][amount];
     }
 };
