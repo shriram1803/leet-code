24860f89593f6959a8401d3fdc898aff9c80ef3c
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-05-29 13:33:51 +0530

    Time: 231 ms (8.96%), Space: 46 MB (18.20%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,22 +3,22 @@ public:
     int backtrack(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp) {        
         if(amt == 0)
             return 0;
-        if(ind == 0) {
-            if(amt % coins[0] == 0)
-                return amt / coins[0];
+        if(ind == 0 and amt < coins[0]) 
             return 1e9;
-        }
         
         if(dp[ind][amt] != -1) 
             return dp[ind][amt];
         
-        int not_pick = backtrack(ind - 1, amt, coins, dp);
+        int not_pick = 1e9;
         int pick_stay = 1e9;
         int pick_move = 1e9;
         if(amt >= coins[ind]) {
             pick_stay = backtrack(ind, amt - coins[ind], coins, dp);
-            pick_move = backtrack(ind - 1, amt - coins[ind], coins, dp);
+            if(ind > 0)
+                pick_move = backtrack(ind - 1, amt - coins[ind], coins, dp);
         }
+        if(ind > 0)
+            not_pick = backtrack(ind - 1, amt, coins, dp);
         
         return dp[ind][amt] = min(not_pick, 1 + min(pick_stay, pick_move));
     }
