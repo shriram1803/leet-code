e745d7c1493450e57d4b237e3c1fad5c9af8ff60
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-03 21:30:55 +0530

    Time: 772 ms (39.00%), Space: 207.1 MB (45.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int backtrack(int ind, int n, bool canBuy, int rem, vector<int>& prices, vector<vector<vector<int>>>& dp) {
-        if(ind == n)
-            return 0;
-        if(rem == 0)
-            return 0;
-        
-        if(dp[ind][canBuy][rem] != -1)
-            return dp[ind][canBuy][rem];
-        
-        if(canBuy) {
-            return dp[ind][canBuy][rem] = max(backtrack(ind + 1, n, canBuy, rem, prices, dp), -prices[ind] + backtrack(ind + 1, n, !canBuy, rem, prices, dp));
-        }
-        
-        return dp[ind][canBuy][rem] = max(backtrack(ind + 1, n, canBuy, rem, prices, dp), prices[ind] + backtrack(ind + 1, n, !canBuy, rem - 1, prices, dp));
-    }
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
         
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
+        const bool canBuy = 1;
         
-        return backtrack(0, n, 1, 2, prices, dp);
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        
+        for(int ind = n - 1; ind >= 0; --ind) {
+            for(int rem = 1; rem <= 2; ++rem) {
+                dp[ind][canBuy][rem] = max(dp[ind + 1][canBuy][rem], -prices[ind] + dp[ind + 1][!canBuy][rem]);
+                
+                dp[ind][!canBuy][rem] = max(dp[ind + 1][!canBuy][rem], prices[ind] + dp[ind + 1][canBuy][rem - 1]);
+            }
+        } 
+        
+        return dp[0][1][2];
     }
 };
