665abc6b0630db570fdcf9586d9047ff18e012e1
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-03 20:11:30 +0530

    Time: 76 ms (53.07%), Space: 6.7 MB (94.06%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,24 +3,27 @@ public:
     bool isMatch(string s, string p) {
         int m = s.size(), n = p.size();
         
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, 0));
+        vector<bool> prev(n + 1, 0), curr(n + 1);
         
-        dp[0][0] = 1;
+        prev[0] = 1;
         for(int i = 1; i <= n; ++i) {
             if(p[i - 1] != '*')
                 break;
-            dp[0][i] = 1;
+            prev[i] = 1;
         }
         
         for(int i = 1; i <= m; ++i) {
             for(int j = 1; j <= n; ++j) {                    
                 if(s[i - 1] == p[j - 1] or p[j - 1] == '?')
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 else if(p[j - 1] == '*')
-                    dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+                    curr[j] = prev[j] | curr[j - 1];
+                else
+                    curr[j] = 0;
             }
+            prev = curr;
         }
         
-        return dp[m][n];
+        return prev[n];
     }
 };
