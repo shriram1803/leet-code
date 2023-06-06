1c44e99838edeff45c86bd6f66b49ba1de70ffba
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-06 22:46:32 +0530

    Time: 6 ms (78.35%), Space: 7.6 MB (93.76%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    bool backtrack(int ind, string curr, string& s, int n, unordered_set<string>& st, vector<unordered_map<string, bool>>& dp)
+    bool backtrack(int ind, string& s, int n, vector<string>& dict, vector<int>& dp)
     {
         if(ind == n)
-            return !curr.size();
+            return 1;
         
-        curr += s[ind];
+        if(dp[ind] != -1)
+            return dp[ind];
         
-        if(dp[ind].count(curr))
-            return dp[ind][curr];
-        
-        if(st.count(curr)) {
-            return dp[ind][curr] = backtrack(ind + 1, "", s, n, st, dp) | backtrack(ind + 1, curr, s, n, st, dp);
+       
+        for(auto& word : dict) {
+            if(s.substr(ind, word.size()) == word)
+                if(backtrack(ind + word.size(), s, n, dict, dp))
+                    return dp[ind] = 1;
         }
         
-        return dp[ind][curr] = backtrack(ind + 1, curr, s, n, st, dp);
+        
+        return dp[ind] = 0;
         
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> st(wordDict.begin(), wordDict.end());
         
-        vector<unordered_map<string, bool>> dp(s.size()); 
+        vector<int> dp(s.size(), -1); 
         
-        return backtrack(0, "", s, s.size(), st, dp);
+        return backtrack(0, s, s.size(), wordDict, dp);
     }
 };
