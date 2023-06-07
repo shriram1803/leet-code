cbe80b1ecef62fd77dc9a7df5de5e1cdc1ea2fa9
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-07 11:30:13 +0530

    Time: 315 ms (59.08%), Space: 10.5 MB (49.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int backtrack(int ind, int prev_ind, int n, vector<int>& nums, vector<vector<int>>& dp) {
-        if(ind == n) 
-            return 0;
-        
-        if(dp[ind][prev_ind + 1] != -1)
-            return dp[ind][prev_ind + 1];
-        
-        //Not pick case
-        int res = backtrack(ind + 1, prev_ind, n, nums, dp);
-        
-        //Pick case
-        if(prev_ind == -1 or nums[ind] > nums[prev_ind])
-            res = max(res, 1 + backtrack(ind + 1, ind, n, nums, dp));
-        
-        return dp[ind][prev_ind + 1] = res;
-    }
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
         
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        vector<int> dp(n, 1);
+        int maxi = 1;
         
-        return backtrack(0, -1, n, nums, dp);
+        for(int ind = 1; ind < n; ++ind) {
+            for(int prev_ind = 0; prev_ind < ind; ++prev_ind) {
+                if(nums[ind] > nums[prev_ind]) 
+                    dp[ind] = max(dp[ind], 1 + dp[prev_ind]);
+            }
+            maxi = max(maxi, dp[ind]);
+        }
+        
+        return maxi;
     }
 };
