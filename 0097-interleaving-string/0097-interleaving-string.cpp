895e4b7c9dcd61c873eb0c290766215b20abc64d
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-07 15:16:24 +0530

    Time: 48 ms (21.31%), Space: 80.1 MB (9.35%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    bool f(int i, int j, int m, int n, int ind, string& a, string& b, string& targ, vector<vector<vector<int>>>& dp) {
-        if(i == m and j == n) 
+    bool f(int i, int j, int ind, string& a, string& b, string& targ, vector<vector<vector<int>>>& dp) {
+        
+        if(i == 0 and j == 0) 
             return 1;
+        
+        if(i == 0) 
+            return b.substr(0, j) == targ.substr(0, j);
+        
+        if(j == 0) 
+            return a.substr(0, i) == targ.substr(0, i);
+            
         
         if(dp[i][j][ind] != -1)
             return dp[i][j][ind];
-
-        if(i == m) {
-            if(b[j++] == targ[ind++])
-                return dp[i][j][ind] = f(i, j, m, n, ind, a, b, targ, dp);
-            return dp[i][j][ind] = 0;
-        }
         
-        if(j == n) {
-            if(a[i++] == targ[ind++])
-                return dp[i][j][ind] = f(i, j, m, n, ind, a, b, targ, dp);
-            return 0;
-        }
+        if(targ[ind] == a[i - 1] and targ[ind] == b[j - 1]) 
+            return dp[i][j][ind] = f(i - 1, j, ind - 1, a, b, targ, dp) | f(i, j - 1, ind - 1, a, b, targ, dp);
         
-        if(a[i] == b[j] and b[j] == targ[ind]) 
-            return dp[i][j][ind] = f(i + 1, j, m, n, ind + 1, a, b, targ, dp) | f(i, j + 1, m, n, ind + 1, a, b, targ, dp);
+        if(targ[ind] == a[i - 1])
+            return dp[i][j][ind] = f(i - 1, j, ind - 1, a, b, targ, dp);
         
-        if(a[i] == targ[ind])
-            return dp[i][j][ind] = f(i + 1, j, m, n, ind + 1, a, b, targ, dp);
-        
-        if(b[j] == targ[ind]) 
-            return dp[i][j][ind] = f(i, j + 1, m, n, ind + 1, a, b, targ, dp);
+        if(targ[ind] == b[j - 1])
+            return dp[i][j][ind] = f(i, j - 1, ind - 1, a, b, targ, dp);
         
         return dp[i][j][ind] = 0;
+        
     }
     bool isInterleave(string s1, string s2, string s3) {
         int m = s1.size(), n = s2.size(), o = s3.size();
@@ -36,6 +33,6 @@ public:
         vector<vector<vector<int>>> dp(m + 1, 
                                       vector<vector<int>>(n + 1,
                                                         vector<int>(o + 1, -1)));
-        return f(0, 0, m, n, 0, s1, s2, s3, dp);
+        return f(m, n, o - 1, s1, s2, s3, dp);
     }
 };
