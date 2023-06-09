9eaa6de834ca6933a1e67f2e4dd671d1b366d774
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-09 13:45:58 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,26 +9,23 @@ using namespace std;
 
 class Solution{
 public:
-    int f(int front, int back, int *arr, vector<vector<int>>& dp) {
-        if(front == back)
-            return 0;
-            
-        if(dp[front][back] != -1)
-            return dp[front][back];
-            
-        int res = 1e9;
-        
-        for(int k = front; k < back; ++k) {
-            res = min(res, arr[front - 1] * arr[back] * arr[k] + f(front, k, arr, dp) + f(k + 1, back, arr, dp));
-        }
-        
-        return dp[front][back] = res;
-    }
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-        vector<vector<int>> dp(N, vector<int>(N, -1));
-        return f(1, N - 1, arr, dp);
+        vector<vector<int>> dp(N, vector<int>(N, 1e9));
+        for(int i = 0; i < N; ++i) dp[i][i] = 0;
+        
+        for(int front = N - 1; front > 0; --front) {
+            for(int back = front + 1; back < N; ++back) {
+                for(int k = front; k < back; ++k) {
+                    dp[front][back] = min(dp[front][back], arr[front - 1]*arr[back]*arr[k]+dp[front][k]+dp[k+1][back]);
+                }
+            }
+        }
+        
+        
+        return dp[1][N - 1];
+
     }
 };
 
