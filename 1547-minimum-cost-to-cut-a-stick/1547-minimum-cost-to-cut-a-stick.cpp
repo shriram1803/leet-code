7395ab5edfa89d34c2c972ddb098a414918decd0
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-10 17:09:25 +0530

    Time: 83 ms (56.97%), Space: 10.4 MB (9.19%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,26 +1,5 @@
 class Solution {
 public:
-    int f(int front, int back, vector<int>& cuts, vector<vector<int>>& dp) {
-        
-        if(front > back)
-            return 0;
-        
-        if(dp[front][back] != -1)
-            return dp[front][back];
-        
-        int res = 1e9;
-        
-        for(int k = front; k <= back; ++k) {
-            res = min(res, 
-                      cuts[back + 1] - 
-                      cuts[front - 1] + 
-                      f(front, k - 1, cuts, dp) + 
-                      f(k + 1, back, cuts, dp)
-                     );
-        }
-        
-        return dp[front][back] = res;
-    }
     int minCost(int n, vector<int>& cuts) {
         
         cuts.emplace_back(0);
@@ -29,8 +8,29 @@ public:
         
         int N = cuts.size();
         
-        vector<vector<int>> dp(N + 1, vector<int>(N + 1, -1));
+        vector<vector<int>> dp(N + 1, vector<int>(N + 1, 1e9));
         
-        return f(1, N - 2, cuts, dp);
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < i; ++j) {
+                dp[i][j] = 0;
+            }
+        }
+        
+        for(int front = N - 2; front > 0; --front) {
+            for(int back = front; back < N - 1; ++back) {
+                int res = 1e9;
+                for(int k = front; k <= back; ++k) {
+                    res = min(res, 
+                      cuts[back + 1] - 
+                      cuts[front - 1] + 
+                      dp[front][k - 1] + 
+                      dp[k + 1][back]
+                     );
+                }
+                dp[front][back] = res;
+            }
+        }
+        
+        return dp[1][N - 2];
     }
 };
