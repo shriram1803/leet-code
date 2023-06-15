cc74b00914e088ffef272c6b0d70e6c9fbe86d88
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-15 20:45:02 +0530

    Time: 485 ms (58.72%), Space: 10.3 MB (41.20%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,24 +1,5 @@
 class Solution {
 public:
-    int f(int front, int back, vector<int>& nums, vector<vector<int>>& dp) {
-        
-        if(front > back) 
-            return 0;
-        
-        if(dp[front][back] != -1)
-            return dp[front][back];
-        
-        int res = -1e9;
-        
-        for(int k = front; k <= back; ++k) {
-            res = max(res, 
-                      nums[k] * nums[front - 1] * nums[back + 1] + 
-                      f(front, k - 1, nums, dp) + f(k + 1, back, nums, dp)
-                     );
-        }
-        
-        return dp[front][back] = res;
-    }
     int maxCoins(vector<int>& nums) {
         
         int n = nums.size();
@@ -26,8 +7,19 @@ public:
         nums.insert(nums.begin(), 1);
         nums.emplace_back(1);
         
-        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
         
-        return f(1, n, nums, dp);
+        for(int front = n; front > 0; --front) {
+            for(int back = 1; back <= n; ++back) {
+                for(int k = front; k <= back; ++k) {
+                    dp[front][back] = max(dp[front][back], 
+                      nums[k] * nums[front - 1] * nums[back + 1] + 
+                      dp[front][k - 1] + dp[k + 1][back]
+                     );
+                }
+            } 
+        }
+        
+        return dp[1][n];
     }
 };
