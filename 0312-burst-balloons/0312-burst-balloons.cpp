d57360378f0f7697afe0eb2cc3dd6230470ae52f
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-15 20:46:32 +0530

    Time: 496 ms (57.06%), Space: 10.3 MB (75.29%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -10,7 +10,7 @@ public:
         vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
         
         for(int front = n; front > 0; --front) {
-            for(int back = 1; back <= n; ++back) {
+            for(int back = front; back <= n; ++back) {
                 for(int k = front; k <= back; ++k) {
                     dp[front][back] = max(dp[front][back], 
                       nums[k] * nums[front - 1] * nums[back + 1] + 
