2d7aff83ac00c728bdb483e87e8303ceff2d13f0
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-20 21:00:01 +0530

    Time: 213 ms (56.44%), Space: 81.4 MB (46.87%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> left(n, 1), right(n, 1);
+        vector<int> left(n), right(n);
         stack<int> s, s1;
         for(int i = 0; i < n; ++i) {
             while(!s.empty() and heights[s.top()] >= heights[i]) {
-                left[i] += left[s.top()];
                 s.pop();
             }
+            if(!s.empty())
+                left[i] = s.top() + 1;
+            else
+                left[i] = 0;
             s.push(i);
         }
         for(int i = n - 1; i >= 0; --i) {
             while(!s1.empty() and heights[s1.top()] >= heights[i]) {
-                right[i] += right[s1.top()];
                 s1.pop();
             }
+            if(!s1.empty())
+                right[i] = s1.top() - 1;
+            else
+                right[i] = n - 1;
             s1.push(i);
         }
         int res = 0;
         for(int i = 0; i < n; ++i) {
-            res = max(res, heights[i] * (left[i] + right[i] - 1));
+            res = max(res, heights[i] * (right[i] - left[i] + 1));
         }
         return res;
     }
