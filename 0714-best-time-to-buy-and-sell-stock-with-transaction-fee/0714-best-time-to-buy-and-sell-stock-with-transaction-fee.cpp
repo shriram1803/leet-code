5ec641ecd5e9241c13d1269822a9c9b4f57a7d2e
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-22 21:46:59 +0530

    Time: 265 ms (27.43%), Space: 90.8 MB (47.11%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -3,19 +3,22 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
         
-        const bool canBuy = 1, cannotBuy = 0;
-        
-        vector<int> prev(2, 0), curr(2, 0);
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
         
         for(int ind = n - 1; ind >= 0; --ind) {
-            curr[canBuy] = max(prev[canBuy], -prices[ind] +  prev[cannotBuy]);
-            
-            // Similar to Stock II - Just subtracted the transaction fee 
-            curr[cannotBuy] = max(prev[cannotBuy], prices[ind] - fee +  prev[canBuy]);
-            
-            prev = curr;
+            for(int canBuy = 0; canBuy <= 1; ++canBuy) {
+                if(canBuy) {
+                    int not_pick = dp[ind + 1][1];
+                    int pick = -prices[ind] - fee + dp[ind + 1][0];
+                    dp[ind][canBuy] = max(not_pick, pick);
+                } else {
+                    int not_pick = dp[ind + 1][0];
+                    int pick = prices[ind] + dp[ind + 1][1];
+                    dp[ind][canBuy] = max(not_pick, pick);
+                }                
+            }
         }
         
-        return prev[1];
+        return dp[0][1];
     }
 };
