3021efb1c293b667e7c170f5d2ca30e94e54f884
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-24 11:10:06 +0530

    Time: 164 ms (77.13%), Space: 9 MB (37.34%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    void fillPalDP(int& n, string& s, vector<vector<int>>& dp) {
-        
-        for(int front = n - 1; front >= 0; --front) {
-            for(int back = max(0, front - 1); back < n; ++back) {
-                if(back <= front) {
-                    dp[front][back] = 1;
-                    continue;
-                }
-                if(s[front] == s[back]) {
-                    dp[front][back] = dp[front + 1][back - 1];   
-                }                    
-            }
-        }
-    }
     int minCut(string s) {
         int n = s.size();
         
-        vector<vector<int>> palDP(n, vector<int>(n, 0));        
-        fillPalDP(n, s, palDP);
+        vector<vector<bool>> isPal(n, vector<bool>(n, 0));  
+        for(int front = n - 1; front >= 0; --front) {
+            for(int back = max(0, front - 1); back < n; ++back) {
+                //When front == back - 1 -> the next step back becomes less than front
+                if(back <= front) {
+                    isPal[front][back] = 1;
+                    continue;
+                }
+                if(s[front] == s[back]) {
+                    isPal[front][back] = isPal[front + 1][back - 1];   
+                }                    
+            }
+        }
         
         vector<int> dp(n + 1, 1e9);
         dp[n] = 0;
         
         for(int ind = n - 1; ind >= 0; --ind) {
             for(int k = ind; k < n; ++k) {
-                if(palDP[ind][k])
+                if(isPal[ind][k])
                     dp[ind] = min(dp[ind], 1 + dp[k + 1]);
             }
         }
