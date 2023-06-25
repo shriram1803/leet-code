9c154ddffe32248d4247f9360afe2dbb54dd1007
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-25 17:56:31 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (72.97%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -3,8 +3,8 @@ public:
     int numDecodings(string s) {
         int n = s.size();
         
-        vector<int> dp(n + 1);
-        dp[n] = 1;
+        vector<int> dp(2);
+        dp[0] = 1;
         
         for(int ind = n - 1; ind >= 0; --ind) {
             int ans = 0;
@@ -13,9 +13,9 @@ public:
                 if(s[ind] == '0')
                     continue;
                 if(0 < curr and curr <= 26) 
-                    ans += dp[ind + k];
+                    ans += dp[k - 1];
             }
-            dp[ind] = ans;
+            dp[1] = dp[0], dp[0] = ans;
         }
         
         return dp[0];
