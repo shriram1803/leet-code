420d025c22086ac1757bf074b7d4e7e2139436e2
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-26 21:26:31 +0530

    Time: 18 ms (52.04%), Space: 13 MB (59.49%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-    int f(int ind, const int n, vector<int>& arr, unordered_map<int, int>& m, vector<int>& dp) {
-        if(ind == n)
-            return 0;
-        
-        if(dp[ind] != -1)
-            return dp[ind];
-        
-        int pick = m[arr[ind]] * arr[ind];
-        if(ind == n - 1 or arr[ind + 1] != arr[ind] + 1) 
-            pick += f(ind + 1, n, arr, m, dp);
-        else
-            pick += f(ind + 2, n, arr, m, dp);
-        
-        int not_pick = f(ind + 1, n, arr, m, dp);
-        
-        return dp[ind] = max(pick, not_pick);
-    }
     int deleteAndEarn(vector<int>& nums) {
         unordered_map<int, int> m;
         vector<int> arr;
@@ -28,9 +11,22 @@ public:
         }
         
         int n = arr.size();
-        vector<int> dp(n, - 1);
+        vector<int> dp(n + 1);
         sort(arr.begin(), arr.end());
         
-        return f(0, n, arr, m, dp);
+        for(int ind = n - 1; ind >= 0; --ind) {
+            
+            int pick = m[arr[ind]] * arr[ind];
+            if(ind == n - 1 or arr[ind + 1] != arr[ind] + 1) 
+                pick += dp[ind + 1];
+            else
+                pick += dp[ind + 2];
+
+            int not_pick = dp[ind + 1];
+            
+            dp[ind] = max(pick, not_pick);
+        }
+        
+        return dp[0];
     }
 };
