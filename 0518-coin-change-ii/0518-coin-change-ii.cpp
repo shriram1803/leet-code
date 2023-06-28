e65f1d6ca1da3ec2560dc0a192585e3ed5830c88
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-28 08:08:49 +0530

    Time: 51 ms (35.55%), Space: 18.3 MB (56.48%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int f(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp) {
-        if(ind == 0)
-            return amt % coins[0] == 0;
-        
-        if(dp[ind][amt] != -1)
-            return dp[ind][amt];
-        
-        //Not pick case
-        int res = f(ind - 1, amt, coins, dp);
-        
-        //Pick case
-        if(amt >= coins[ind])
-            res += f(ind, amt - coins[ind], coins, dp);
-        
-        return dp[ind][amt] = res;
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        vector<vector<int>> dp(n, vector<int>(amount + 1));
         
-        return f(n - 1, amount, coins, dp);
+        for(int amt = 0; amt <= amount; ++amt) 
+            if(amt % coins[0] == 0)
+                dp[0][amt] = 1;
+        
+        for(int ind = 1; ind < n; ++ind) {
+            for(int amt = 0; amt <= amount; ++amt) {
+                
+                //Not pick case
+                int res = dp[ind - 1][amt];
+
+                //Pick case
+                if(amt >= coins[ind])
+                    res += dp[ind][amt - coins[ind]];
+
+                dp[ind][amt] = res;
+            }
+        }
+        
+        return dp[n - 1][amount];
     }
 };
