70523e1e4074ce0bcd6497ef2b60af2ec7e2203c
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-28 19:41:19 +0530

    Time: 22 ms (57.39%), Space: 12.1 MB (43.53%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    int f(int ind, int rem, vector<int>& nums, vector<vector<int>>& dp) {
-        if(ind == 0)
-            return (rem == 0) + (rem == nums[0]);
-        
-        if(dp[ind][rem] != -1)
-            return dp[ind][rem];
-        
-        //Not pick case
-        int res = f(ind - 1, rem, nums, dp);
-        
-        //Pick Case
-        if(rem >= nums[ind]) 
-            res += f(ind - 1, rem - nums[ind], nums, dp);
-        
-        return dp[ind][rem] = res;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         
@@ -26,8 +10,27 @@ public:
         
         target = (sum - target) / 2;
         
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        vector<vector<int>> dp(n, vector<int>(target + 1));
         
-        return f(n - 1, target, nums, dp);
+        dp[0][0] += 1;
+        if(nums[0] <= target)
+            dp[0][nums[0]] += 1;
+        for(int ind = 1; ind < n; ++ind) {
+            for(int rem = 0; rem <= target; ++rem) {
+                
+                //Not pick case
+                int res = dp[ind - 1][rem];
+
+                //Pick Case
+                if(rem >= nums[ind]) 
+                    res += dp[ind - 1][rem - nums[ind]];
+
+                dp[ind][rem] = res;
+                
+            }
+        }
+        
+        
+        return dp[n - 1][target];
     }
 };
