2ee421330a955571eb7f9440e53cb8ab80bf47bf
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-28 22:02:32 +0530

    Time: 8 ms (37.79%), Space: 7.2 MB (27.07%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
         
-        if(i < 0 and j < 0) {
+        if(i == 0 and j == 0) {
             return 1;
         }
         
-        if(i < 0) {
-            while(j >= 0) {
-                if(p[j] != '*')
+        if(i == 0) {
+            while(j > 0) {
+                if(p[j - 1] != '*')
                     return 0;
                 j -= 2;
             }
             return 1;
         }
         
-        if(j < 0)
+        if(j == 0)
             return 0;
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        if(p[j] == '.' or s[i] == p[j]) 
+        if(p[j - 1] == '.' or s[i - 1] == p[j - 1]) 
             return dp[i][j] = f(i - 1, j - 1, s, p, dp);
         
-        if(p[j] == '*') {
-            if(s[i] == p[j - 1] or p[j - 1] == '.')
+        if(p[j - 1] == '*') {
+            if(s[i - 1] == p[j - 2] or p[j - 2] == '.')
                 return dp[i][j] = f(i - 1, j - 1, s, p, dp) | f(i - 1, j, s, p, dp) | f(i, j - 2, s, p, dp);
             else
                 return dp[i][j] = f(i, j - 2, s, p, dp);
@@ -36,8 +36,8 @@ public:
     bool isMatch(string s, string p) {
         int m = s.size(), n = p.size();
         
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
         
-        return f(m - 1, n - 1, s, p, dp);
+        return f(m, n, s, p, dp);
     }
 };
