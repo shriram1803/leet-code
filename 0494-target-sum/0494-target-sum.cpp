46ff2513c126d77d123f3c7b72ceb86cca1dd530
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-28 19:35:34 +0530

    Time: 11 ms (87.79%), Space: 11.9 MB (55.49%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int total = accumulate(nums.begin(), nums.end(), 0), n = nums.size();
+    int f(int ind, int rem, vector<int>& nums, vector<vector<int>>& dp) {
+        if(ind == 0)
+            return (rem == 0) + (rem == nums[0]);
         
-        if(total < target or (total - target) % 2 == 1) 
+        if(dp[ind][rem] != -1)
+            return dp[ind][rem];
+        
+        //Not pick case
+        int res = f(ind - 1, rem, nums, dp);
+        
+        //Pick Case
+        if(rem >= nums[ind]) 
+            res += f(ind - 1, rem - nums[ind], nums, dp);
+        
+        return dp[ind][rem] = res;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        
+        if(target > sum or (sum - target) % 2 == 1)
             return 0;
         
-        target = (total - target) / 2;
+        target = (sum - target) / 2;
         
-        vector<int> dp(target + 1, 0);
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
         
-        dp[0] = 1;
-        if(target >= nums[0])
-            dp[nums[0]] += 1;
-        
-        for(int ind = 1; ind < n; ++ind) {
-            for(int targ = target; targ >= 0; --targ) {
-                int not_pick = dp[targ];
-                int pick = 0;
-                if(targ >= nums[ind])
-                    pick += dp[targ - nums[ind]];
-                dp[targ] = pick + not_pick;
-            }
-        }
-        
-        return dp[target];
+        return f(n - 1, target, nums, dp);
     }
 };
