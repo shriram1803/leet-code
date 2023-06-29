96d4337f90fef371588e4131e89f6fd6cc5c18b4
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-29 08:33:40 +0530

    Time: 20 ms (7.61%), Space: 48.2 MB (5.11%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     const int mod = (int)(1e9 + 7);
-    int f(int up, int down, vector<vector<int>>& dp) {
+    long long f(int up, int down, vector<vector<int>>& dp) {
         if(up == 0 and down == 0)
             return 1;
-        if(up < 0 or down < 0)
+        if(up <= 0 or down <= 0)
             return 0;
         
         if(dp[up][down] != -1)
@@ -12,18 +12,13 @@ public:
         
         long long res = 0;
         
-        if(up == down)
-            res += (f(up - 1, down - 1, dp) % mod + f(up - 2, down - 2, dp) % mod) % mod;
-        
-        if(up - 1 == down)
-            res += f(up - 2, down, dp) % mod;
         if(up == down - 1)
-            res += f(up, down - 2, dp) % mod;
-        
-        if(up == down or up - 1 == down)
-            res += f(up - 2, down - 1, dp) % mod;
-        if(up == down or down - 1 == up)
-            res += f(up - 1, down - 2, dp) % mod;
+            res += (f(up, down - 2, dp) % mod + f(up - 1, down - 2, dp) % mod) % mod;
+        else if(up - 1 == down)
+            res += (f(up - 2, down, dp) % mod + f(up - 2, down - 1, dp) % mod) % mod;
+        else
+            res += (f(up - 1, down - 1, dp) % mod + f(up - 2, down - 2, dp) % mod + 
+                   f(up - 2, down - 1, dp) % mod + f(up - 1, down - 2, dp) % mod) % mod;
         
         return dp[up][down] = res % mod;
     }
