6e3bce6c4bae070435330a7e32dc1a8c442fab2a
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-29 23:01:21 +0530

    Time: 1011 ms (6.20%), Space: 102.7 MB (25.82%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int f(int ind, int i, int j, vector<string>& strs, vector<vector<vector<int>>>& dp) {
-        if(ind < 0)
-            return 0;
-        if(i == 0 and j == 0)
-            return 0;
-        
-        if(dp[ind][i][j] != -1)
-            return dp[ind][i][j];
-        
-        int not_pick = f(ind - 1, i, j, strs, dp);
-        
-        int zeroes = 0;
-        for(int k = 0; k < strs[ind].size(); ++k) {
-            if(strs[ind][k] == '0')
-                zeroes += 1;
-        }
-        
-        int pick = -1e9;
-        if(i >= zeroes and j >= (strs[ind].size() - zeroes))
-            pick = 1 + f(ind - 1, i - zeroes, j - (strs[ind].size() - zeroes), strs, dp);
-        
-        return dp[ind][i][j] = max(not_pick, pick);
-    }
     int findMaxForm(vector<string>& strs, int m, int n) {
         int N = strs.size();
         
-        vector<vector<vector<int>>> dp(N, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
+        vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(m + 1, vector<int>(n + 1)));
         
-        return f(N - 1, m, n, strs, dp);
+        for(int ind = 1; ind <= N; ++ind) {
+            for(int i = 0; i <= m; ++i) {
+                for(int j = 0; j <= n; ++j) {
+                    
+                    int not_pick = dp[ind - 1][i][j];
+                    int zeroes = 0;
+                    for(int k = 0; k < strs[ind - 1].size(); ++k) {
+                        if(strs[ind - 1][k] == '0')
+                            zeroes += 1;
+                    }
+
+                    int pick = -1e9;
+                    if(i >= zeroes and j >= (strs[ind - 1].size() - zeroes))
+                        pick = 1 + dp[ind - 1][i - zeroes][j - (strs[ind - 1].size() - zeroes)];
+
+                    dp[ind][i][j] = max(not_pick, pick);
+                }
+            }
+        }
+        
+        return dp[N][m][n];
     }
 };
