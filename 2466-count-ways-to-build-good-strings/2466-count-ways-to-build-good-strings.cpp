bc67a8de16b06a551f6104bb84c1ffe921c5c6d4
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-30 22:42:07 +0530

    Time: 20 ms (40.99%), Space: 9.5 MB (65.65%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     const int mod = (int)(1e9 + 7);
-    int f(int curr, int zero, int one, int low, int high, vector<int>& dp) {
-        if(curr > high)
-            return 0;
-        
-        if(dp[curr] != -1)
-            return dp[curr];
-        
-        int res = 0;
-        if(curr >= low)
-            res += 1;
-        
-        return dp[curr] = (res + f(curr + one, zero, one, low, high, dp) % mod + f(curr + zero, zero, one, low, high, dp) % mod) % mod;
-    }
     int countGoodStrings(int low, int high, int zero, int one) {
         
-        vector<int> dp(high + 1, -1);
+        vector<int> dp(high + 1);
         
-        return f(0, zero, one, low, high, dp);
+        for(int curr = high; curr >= 0; --curr) {
+        
+            dp[curr] = (
+                (curr + one <= high ? dp[curr + one] : 0) % mod 
+                + 
+                (curr + zero <= high ? dp[curr + zero] : 0) % mod
+            ) % mod;
+            
+            if(curr >= low)
+                dp[curr] += 1;
+        }
+        
+        return dp[0];
     }
 };
