a76a2f47c1768ea767d1ce29545e57945ca9854d
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-30 22:11:36 +0530

    Time: 5 ms (43.35%), Space: 9.6 MB (80.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     vector<int> pass{1, 7, 30};
-    int f(int ind, const int n, vector<int>& days, vector<int>& costs, vector<int>& dp) {
-        if(ind == n)
-            return 0;
-        
-        if(dp[ind] != -1)
-            return dp[ind];
-        
-        int res = 1e9;
-        
-        for(int i = 0; i < 3; ++i) {
-            int k;
-            for(k = ind + 1; k < n and (days[ind] + pass[i] - 1 >= days[k]); ++k); 
-            res = min(res, costs[i] + f(k, n, days, costs, dp));
-        }
-        
-        return dp[ind] = res;
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         
-        vector<int> dp(n, -1);
+        vector<int> dp(n + 1, 1e9);
+        dp[n] = 0;
         
-        return f(0, n, days, costs, dp);
+        for(int ind = n - 1; ind >= 0; --ind) {
+            for(int i = 0; i < 3; ++i) {
+                int k;
+                for(k = ind + 1; k < n and (days[ind] + pass[i] - 1 >= days[k]); ++k); 
+                dp[ind] = min(dp[ind], costs[i] + dp[k]);
+            }
+        }
+        
+        return dp[0];
     }
 };
