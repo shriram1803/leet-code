6deeb2de84058cd7ee8dac8bdd25aded9865bc0f
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-30 21:34:16 +0530

    Time: 36 ms (84.10%), Space: 15.1 MB (70.61%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public: 
-    int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
-        if(i < 0 and j < 0)
-            return 0;
-        if(i < 0) {
-            int s = 0;
-            while(j >= 0)
-                s += s2[j--];
-            return s;
-        }
-        if(j < 0) {
-            int s = 0;
-            while(i >= 0)
-                s += s1[i--];
-            return s;
-        }
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        if(s1[i] == s2[j])
-            return dp[i][j] = f(i - 1, j - 1, s1, s2, dp);
-        
-        return dp[i][j] = min(s1[i] + f(i - 1, j, s1, s2, dp), s2[j] + f(i, j - 1, s1, s2, dp));
-    }
     int minimumDeleteSum(string s1, string s2) {
         int m = s1.size(), n = s2.size();
         
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         
-        return f(m - 1, n - 1, s1, s2, dp);
+        for(int i = 1; i <= m; ++i)
+            dp[i][0] += dp[i - 1][0] + s1[i - 1];
+
+        for(int j = 1; j <= n; ++j)
+            dp[0][j] += dp[0][j - 1] + s2[j - 1];
+        
+        
+        for(int i = 1; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j) {
+                if(s1[i - 1] == s2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = min(s1[i - 1] + dp[i - 1][j], s2[j - 1] + dp[i][j - 1]);
+            }
+        }
+        
+        
+        return dp[m][n];
     }
 };
