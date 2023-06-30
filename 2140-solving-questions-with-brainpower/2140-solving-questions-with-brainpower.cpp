31c1b1b663136292acc4e157ffee59fc63d51eb9
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-06-30 08:37:23 +0530

    Time: 400 ms (49.70%), Space: 115.1 MB (89.26%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    long long f(int ind, const int n, vector<vector<int>>& q, vector<long long>& dp) {
-        if(ind == n)
-            return 0;
-        
-        if(dp[ind] != -1)
-            return dp[ind];
-        
-        long long not_pick = f(ind + 1, n, q, dp);
-        
-        long long pick = q[ind][0];
-        if(ind + q[ind][1] < n)
-            pick += f(ind + q[ind][1] + 1, n, q, dp);
-        
-        
-        return dp[ind] = max(not_pick, pick);
-    }
     long long mostPoints(vector<vector<int>>& q) {
         int n = q.size();
         
-        vector<long long> dp(n, -1);
+        vector<long long> dp(n + 1);
         
-        return f(0, n, q, dp);
+        for(int ind = n - 1; ind >= 0; --ind) {
+            
+            dp[ind] = max(dp[ind + 1], (long long)q[ind][0]); //
+            if(ind + q[ind][1] < n)
+                dp[ind] = max(dp[ind], q[ind][0] + dp[ind + q[ind][1] + 1]);
+            
+        }
+        
+        return dp[0];
     }
 };
