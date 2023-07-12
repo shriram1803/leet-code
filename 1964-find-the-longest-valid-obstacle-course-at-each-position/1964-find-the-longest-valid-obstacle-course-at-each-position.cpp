f5846de9815aa33c89d824e46e5c7e04c504ae52
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-07-12 21:51:57 +0530

    Time: 338 ms (56.43%), Space: 123.3 MB (15.01%) - LeetHub

--- 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
-        int n = obstacles.size();
-        vector<int> res, dp(n + 1, INT_MAX);
-        for(int obstacle : obstacles) {
-            int ind = upper_bound(dp.begin(), dp.end(), obstacle) - dp.begin();
-            res.emplace_back(ind + 1);
-            dp[ind] = min(dp[ind], obstacle);
+        vector<int> monotonic;
+        vector<int> res;
+        
+        for(int obs : obstacles) {
+            if(monotonic.empty() or monotonic.back() <= obs) {
+                monotonic.emplace_back(obs);
+                res.emplace_back(monotonic.size());
+            } else {
+                int ind = upper_bound(monotonic.begin(), monotonic.end(), obs) - monotonic.begin();
+                res.emplace_back(ind + 1);
+                monotonic[ind] = obs;
+            }
         }
+        
         return res;
     }
 };
