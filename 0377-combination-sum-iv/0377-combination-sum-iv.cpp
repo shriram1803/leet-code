c92a93562c80d684f8a30b69baadb0d094b28192
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-07-12 21:34:09 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (42.85%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
+    int f(vector<int>& nums, int rem, vector<int>& dp) {
+        if(rem == 0)
+            return 1;
+        
+        if(dp[rem] != -1)
+            return dp[rem];
+        
+        int res = 0;
+        for(auto& num : nums) 
+            if(num <= rem)
+                res += f(nums, rem - num, dp);
+        
+        
+        return dp[rem] = res;
+    } 
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned int> dp(target + 1, 0); 
-        dp[0] = 1;
-        for(int i = 1; i <= target; ++i) {
-            for(int num : nums) {
-                if(i < num) continue;
-                dp[i] += dp[i - num]; 
-            }
-        }
-        return dp[target];
+        int n = nums.size();
+        vector<int> dp(target + 1, -1);
+        return f(nums, target, dp);
     }
 };
