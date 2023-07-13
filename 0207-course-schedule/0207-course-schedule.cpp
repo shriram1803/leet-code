b9f95a757a1872d1f24ddfdc9caf7621b9fe586b
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-07-13 09:12:02 +0530

    Time: 19 ms (91.53%), Space: 13.9 MB (52.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    void dfs(int curr, vector<vector<int>>& g, vector<int>& vis, vector<int>& indegree, int& rem_courses) 
+    void dfs(int curr, vector<vector<int>>& g, vector<int>& vis, vector<int>& indegree, int& courses) 
     {
         if(vis[curr] == 1)
             return;
+        
+        //Everytime the node is called -> one of it's pre-requisite has been called
+        //So, when indegree == 1 -> all of it's pre-requisites have been called
         if(indegree[curr] > 1) {
             indegree[curr] -= 1;
             return;
         }
         
-        vis[curr] = 1, rem_courses -= 1;
+        vis[curr] = 1, courses += 1;
         
         for(auto& node : g[curr])
-            dfs(node, g, vis, indegree, rem_courses);
+            dfs(node, g, vis, indegree, courses);
     }
     bool canFinish(int numCourses, vector<vector<int>>& prereq) {
         int n = prereq.size();
@@ -25,13 +28,14 @@ public:
             g[p[1]].emplace_back(p[0]);
         }
         
-        int rem_courses = numCourses;
+        int coursesFinished = 0;
+        //nodes with indegree=0 means they have no pre-requisites
         for(int i = 0; i < numCourses; ++i) {
             if(indegree[i] == 0)
-                dfs(i, g, vis, indegree, rem_courses);
+                dfs(i, g, vis, indegree, coursesFinished);
         }
             
             
-        return rem_courses == 0;
+        return coursesFinished == numCourses;
     }
 };
