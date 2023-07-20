773ae55d6a8716f3cf3ca836797ad68af461a156
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-07-20 08:52:24 +0530

    Time: 12 ms (83.98%), Space: 17.5 MB (81.53%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> stack;
+        for(auto& a : asteroids) {
+            if(a < 0) {
+                while(!stack.empty() and stack.back() > 0 and stack.back() < -1*a) 
+                    stack.pop_back();
+                if(!stack.empty() and stack.back() == -1*a)
+                    stack.pop_back();
+                else {
+                    if(stack.empty() or stack.back() < 0)
+                        stack.emplace_back(a);
+                }                
+            } else {
+                stack.emplace_back(a);
+            }
+        }
+        return stack;
+    }
+};
