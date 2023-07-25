0c624ba4afe7ad12f02e9865ed1813b88a352f38
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-07-25 22:52:20 +0530

    Time: 20 ms (96.53%), Space: 12 MB (72.82%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -3,19 +3,18 @@ public:
     int minimumMountainRemovals(vector<int>& nums) {
         int res = 1e9;
         int n = nums.size();
-        vector<int> left(n, 1);
-        vector<int> right(n, 1);
+        vector<int> lis(n, 1);
         
         //LIS (left[i] => the LIS till i provided i is included)
         vector<int> temp;
         for(int i = 0; i < n; ++i) {
             if(temp.empty() or temp.back() < nums[i]) {
                 temp.emplace_back(nums[i]);
-                left[i] = temp.size();
+                lis[i] = temp.size();
             } else {
                 int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
                 temp[ind] = nums[i];
-                left[i] = ind + 1;
+                lis[i] = ind + 1;
             }
         }
         
@@ -27,18 +26,18 @@ public:
         //Reverse LIS
         temp.clear();
         for(int i = n - 1; i >= 0; --i) {
+            int ind;
             if(temp.empty() or temp.back() < nums[i]) {
                 temp.emplace_back(nums[i]);
-                right[i] = temp.size();
+                ind = temp.size() - 1;
             } else {
-                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
                 temp[ind] = nums[i];
-                right[i] = ind + 1;
             }
             
-            if(left[i] == 1 or right[i] == 1) //check to only consider valid peak indexes
+            if(lis[i] == 1 or ((ind + 1) == 1)) //check to only consider valid peak indexes
                 continue;
-            res = min(res, n - (left[i] + right[i] - 1));
+            res = min(res, n - (lis[i] + ind));
             
         }
         
