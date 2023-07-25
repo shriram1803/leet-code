bc426fbb5a439f5e68fb0f6bfb0e655f1f02e495
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-07-25 21:39:35 +0530

    Time: 132 ms (35.53%), Space: 59.7 MB (40.39%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,8 +5,6 @@ public:
         
         while(left <= right) {
             mid = left + (right - left) / 2;
-            if(arr[mid - 1] < arr[mid] and arr[mid + 1] < arr[mid])
-                return mid;
             if(arr[mid + 1] < arr[mid])
                 right = mid - 1;
             else
