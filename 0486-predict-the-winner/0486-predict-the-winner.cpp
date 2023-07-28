3ae7509261051d04f611e191afa742a3d69322d7
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-07-28 22:12:30 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (75.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,19 +3,15 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> prev(n);
-        vector<int> curr(n);
+        vector<int> dp(n);
         
         for(int front = n - 1; front >= 0; --front) {
-            curr[front] = nums[front];
+            dp[front] = nums[front];
             for(int back = front + 1; back < n; ++back) {
-                        
-                curr[back] = max(nums[front] - prev[back], nums[back] - curr[back - 1]);
-                
+                dp[back] = max(nums[front] - dp[back], nums[back] - dp[back - 1]);                
             }
-            prev = curr;
         }
         
-        return prev[n - 1] >= 0;
+        return dp[n - 1] >= 0;
     }
 };
