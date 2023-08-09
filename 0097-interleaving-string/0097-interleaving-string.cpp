95e28498517bd39f11fd6124659d88af06e9a350
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-08-09 22:08:43 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (58.01%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    bool f(int i, int j, int ind, string& a, string& b, string& targ, vector<vector<vector<int>>>& dp) {
-        
-        if(i == 0 and j == 0) 
+    bool f(int i, int j, string& s1, string& s2, string& s3, vector<vector<int>>& dp) {
+        if(i == 0 and j == 0)
             return 1;
         
-        if(i == 0) 
-            return b.substr(0, j) == targ.substr(0, j);
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
-        if(j == 0) 
-            return a.substr(0, i) == targ.substr(0, i);
-            
+        bool res = 0;
         
-        if(dp[i][j][ind] != -1)
-            return dp[i][j][ind];
+        if(i > 0 and s1[i - 1] == s3[i + j - 1])
+            res = f(i - 1, j, s1, s2, s3, dp) | res;
         
-        if(targ[ind] == a[i - 1] and targ[ind] == b[j - 1]) 
-            return dp[i][j][ind] = f(i - 1, j, ind - 1, a, b, targ, dp) | f(i, j - 1, ind - 1, a, b, targ, dp);
+        if(j > 0 and s2[j - 1] == s3[i + j - 1])
+            res = f(i, j - 1, s1, s2, s3, dp) | res;
         
-        if(targ[ind] == a[i - 1])
-            return dp[i][j][ind] = f(i - 1, j, ind - 1, a, b, targ, dp);
-        
-        if(targ[ind] == b[j - 1])
-            return dp[i][j][ind] = f(i, j - 1, ind - 1, a, b, targ, dp);
-        
-        return dp[i][j][ind] = 0;
-        
+        return dp[i][j] = res;
     }
     bool isInterleave(string s1, string s2, string s3) {
         int m = s1.size(), n = s2.size(), o = s3.size();
-        if(o != m + n) return false;
-        vector<vector<vector<int>>> dp(m + 1, 
-                                      vector<vector<int>>(n + 1,
-                                                        vector<int>(o + 1, -1)));
-        return f(m, n, o - 1, s1, s2, s3, dp);
+        
+        if(m + n != o)
+            return 0;
+        
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        
+        return f(m, n, s1, s2, s3, dp);
     }
 };
