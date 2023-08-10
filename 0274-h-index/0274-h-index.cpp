bf9adfc5be820f03070ed209befd1726268b522c
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-08-10 09:19:18 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (54.29%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,33 +1,14 @@
 class Solution {
 public:
-    bool can(vector<int>& c, int h) {
-        int cnt = 0;
-        for(int num : c) {
-            if(cnt == h)
-                return 1;
-            if(num >= h)
-                cnt += 1;
-        }
-        return cnt == h;
-    }
     int hIndex(vector<int>& c) {
         sort(c.begin(), c.end(), greater<int>());
+        int i;
         
-        int left = 0;
-        int right = c.front();
-        int mid;
-        int ans = 0;
-        
-        while(left <= right) {
-            mid = left + (right - left) / 2;
-            
-            if(can(c, mid))
-                ans = mid, left = mid + 1;
-            else
-                right = mid - 1;
-            
+        for(i = 0; i < c.size(); ++i) {
+            if((i + 1) > c[i])
+                break;
         }
         
-        return ans;
+        return i;
     }
 };
