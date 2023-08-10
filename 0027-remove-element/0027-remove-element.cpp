74458ec6cb076a4a9e6df2b5be483b3d2de070ef
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-08-10 08:48:46 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (45.61%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int front = 0, back = nums.size() - 1;
+        int j = 0;
         
-        while(front <= back) {
-            while(back >= 0 and nums[back] == val) back -= 1;
-            while(front < back and nums[front] != val) front += 1;
-            
-            if(front == back)
-                break;
-            
-            if(front < back)
-                swap(nums[front], nums[back]);
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] != val)
+                swap(nums[i], nums[j++]);
         }
         
-        return back + 1;
+        return j;
     }
 };
