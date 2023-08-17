51bd8fe4d537a9ce8282f797eefa59c05fc0d3c4
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-08-17 10:07:41 +0530

    Time: 65 ms (87.06%), Space: 31.5 MB (73.11%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    vector<int> dirs{-1, 0, 1, 0, -1};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        queue<pair<int, int>> q;
+        deque<array<int, 3>> dq;
+        
         for(int i = 0; i < m; ++i) {
             for(int j = 0; j < n; ++j) {
-                if(mat[i][j] == 0) 
-                    q.push({i, j}), vis[i][j] = 1;                    
+                if(mat[i][j] == 0)
+                    dq.push_back({i, j, 0});
+                else
+                    mat[i][j] = 1e9;
             }
         }
-        if(q.size() == m * n) return mat;
-        while(!q.empty()) {
-            int x = q.front().first, y = q.front().second;
-            q.pop();
+        
+        vector<int> dirs{-1, 0, 1, 0, -1};
+        while(!dq.empty()) {
+            auto [x, y, dst] = dq.front();
+            dq.pop_front();
+            
             for(int i = 0; i <= 3; ++i) {
-                int xx = x + dirs[i], yy = y + dirs[i + 1];                    
-                if(xx < 0 or yy < 0 or xx >= m or yy >= n or vis[xx][yy]) 
+                int cur_x = x + dirs[i], cur_y = y + dirs[i + 1];
+                if(cur_x < 0 or cur_y < 0 or cur_x >= m or cur_y >= n or mat[cur_x][cur_y] <= dst + 1)
                     continue;
-                mat[xx][yy] = mat[x][y] + 1, vis[xx][yy] = 1;
-                q.push({xx, yy});
-            }        
+                mat[cur_x][cur_y] = dst + 1;
+                dq.push_back({cur_x, cur_y, dst + 1});
+            }
+            
         }
+        
         return mat;
     }
 };
