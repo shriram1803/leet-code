886997e01aade3011da6b5456b037ff75a94e2fe
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-08-27 13:31:06 +0530

    Time: 60 ms (89.71%), Space: 210.4 MB (31.72%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool f(int ind, int prev, int n, vector<int>& stones, vector<vector<int>>& dp) {
+        if(ind == n - 1)
+            return 1;
+        
+        if(dp[ind][prev] != -1)
+            return dp[ind][prev];
+        
+        long long prev_jump = stones[ind] - stones[prev];
+        
+        long long max_reach = stones[ind] + prev_jump + 1; 
+        
+        for(int j = ind + 1; j < n and stones[j] <= max_reach; ++j) {
+            
+            for(int k = prev_jump - 1; k <= prev_jump + 1; ++k) {
+                if(stones[j] - stones[ind] == k and f(j, ind, n, stones, dp))
+                    return dp[ind][prev] = 1;
+            }
+            
+        }
+        
+        return dp[ind][prev] = 0; 
+    }
+    bool canCross(vector<int>& stones) {
+        int n = stones.size();
+        
+        if(stones[1] != 1)
+            return 0;
+        
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        
+        return f(1, 0, n, stones, dp);
+    }
+};
