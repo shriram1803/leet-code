8e6fa3d7e0ad72159ed47427fa2180e0e2981a10
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-10-28 13:17:28 +0530

    Time: 411 ms (28.00%), Space: 129.3 MB (27.30%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPal(string& s, int j, int i) {
+        bool res = true;
+        while(res and i < j) {
+            if(s[i] != s[j]) 
+                res = false;
+            ++i;
+            --j;
+        }
+        return res;
+    }
+    string longestPalindrome(string s) {
+        string res = "";
+        int max_res = 1;
+        int n = s.size();
+        res += s[0];
+        
+        for(int i = 0; i < n; ++i) {
+            string str = "";
+            for(int j = i; j < n; ++j) {
+                str += s[j];
+                if(j - i + 1 > max_res && isPal(s, j, i)) {
+                    max_res = j - i + 1;
+                    res = s.substr(i, j - i + 1);
+                }
+            }
+        }
+        
+        return res;
+    }
+};
