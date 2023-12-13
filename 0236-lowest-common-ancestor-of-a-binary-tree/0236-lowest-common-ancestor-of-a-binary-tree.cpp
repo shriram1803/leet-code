e18bf8242d542ca3ce18ee33709bd87e498f8206
Shriram R <83195038+shriram1803@users.noreply.github.com>
2023-12-13 15:25:50 +0530

    Time: 11 ms (75.30%), Space: 17.9 MB (5.39%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,12 +9,35 @@
  */
 class Solution {
 public:
+    pair<TreeNode*, int> dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
+            return {nullptr, 0}; 
+        
+        int local = 0;
+        
+        if(root == p) 
+            local += 1;
+        
+        if(root == q)
+            local += 1;
+        
+        auto left = dfs(root->left, p, q);
+        auto right = dfs(root->right, p, q);
+     
+        local += left.second + right.second;
+        
+        if(local == 2)
+            return {root, 0};
+        
+        if(left.first)
+            return {left.first, 0};
+        
+        if(right.first)
+            return {right.first, 0};        
+        
+        return {nullptr, local};
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root or root == p or root == q) return root;
-        
-        TreeNode* lft = lowestCommonAncestor(root->left, p, q);
-        TreeNode* rht = lowestCommonAncestor(root->right, p, q);
-        
-        return !lft ? rht : !rht ? lft : root;
+        return dfs(root, p, q).first;
     }
 };
